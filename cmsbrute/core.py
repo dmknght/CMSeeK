001279a7d94e233af19b53c178e0611a5fb36231
dmknght <dmknght@localhost.localdomain>
2019-06-14 00:28:59 +0700

    Add get login form info (work with patched mechanicalsoup)

---
+++ cmsbrute/core.py
@@ -0,0 +1,41 @@
+import re
+
+def analysis(form):
+   reTextControl = r"text\((.*)\)"
+   rePasswdControl = r"password\((.*)\)"
+   txtPasswdControl = re.findall(rePasswdControl, form)
+      # Find password control. If has
+      # 	1 password control -> login field
+      # 	2 or more password control -> possibly register field
+   if len(txtPasswdControl) == 1:
+      txtTextControl = re.findall(reTextControl, form)
+      # if len(txtTextControl) == 1:
+            # Regular login field. > 1 can be register specific field (maybe captcha)
+      return (txtTextControl[0], txtPasswdControl[0])
+         # FOR WEB SHELL ONLY. REMOVE BUT CAN BE USED LATER IF NEEDED
+         # elif len(txtTextControl) == 0:
+         #    # Possibly password field login only
+         #    return ([uint_formID, txtSubmitControl[0]], [txtPasswdControl[0]])
+   return False
+
+
+
+def getLoginForm(objBrowser):
+   loginFields = False
+   for fid in range(0, 99):
+      try:
+         objBrowser.select_form(nr = fid)
+         loginFields = analysis(objBrowser.get_current_form().get_info())
+
+         if loginFields:
+            textField, passField = loginFields
+            loginFields = (fid, textField, passField)
+            return loginFields
+      except Exception as error:
+         if fid == 0:
+            print("[x] Found no form")
+         else:
+            print("%s" %(error))
+         break
+      finally:
+         return loginFields
